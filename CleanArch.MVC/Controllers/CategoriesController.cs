95cdc6d13711766a213937223725b7b7fbe26c1c
Gabriel C Silva <gabriel.camposdasilva@hotmail.com>
2022-12-21 16:07:08 -0300

    category edit

--- CleanArch.MVC/Controllers/CategoriesController.cs
+++ CleanArch.MVC/Controllers/CategoriesController.cs
@@ -4,37 +4,66 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArch.MVC.Controllers
 {
-    public class CategoriesController : Controller
+  public class CategoriesController : Controller
+  {
+    private readonly ICategoryService _categoryService;
+    public CategoriesController(ICategoryService categoryService)
     {
-        private readonly ICategoryService _categoryService;
-        public CategoriesController(ICategoryService categoryService)
-        {
-            _categoryService = categoryService;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            var categories = await _categoryService.GetCategories();
-            return View(categories);
-        }
-
-        [HttpGet]
-        public IActionResult Create()
-        {
-            // primeiro apresenta o formulario para o usuario preencher
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> Create(CategoryDTO category)
-        {
-            // Segundo pegar os dados e executar o metodo desejado e retorna o resultado para o usuario
-            if(ModelState.IsValid)
-            {
-                await _categoryService.Add(category);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(category);
-        }
+      _categoryService = categoryService;
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+      var categories = await _categoryService.GetCategories();
+      return View(categories);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+      // primeiro apresenta o formulario para o usuario preencher
+      return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(CategoryDTO category)
+    {
+      // Segundo pegar os dados e executar o metodo desejado e retorna o resultado para o usuario
+      if (ModelState.IsValid)
+      {
+        await _categoryService.Add(category);
+        return RedirectToAction(nameof(Index));
+      }
+      return View(category);
+    }
+    [HttpGet()]
+    public async Task<IActionResult> Edit(int? id)
+    {
+      if (id == null) return NotFound();
+
+      var categoryDTO = await _categoryService.GetCategoryById(id);
+
+      if (categoryDTO == null) return NotFound();
+
+      return View(categoryDTO);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(CategoryDTO categoryDTO)
+    {
+      if (ModelState.IsValid)
+      {
+        try
+        {
+          await _categoryService.Update(categoryDTO);
+        }
+        catch (Exception)
+        {
+          throw;
+        }
+        return RedirectToAction(nameof(Index));
+      }
+      return View(categoryDTO);
+    }
+  }
 }
