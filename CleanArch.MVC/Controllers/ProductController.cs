a753c86144d918c57885a78783fbd5d91abda818
Gabriel C Silva <gabriel.camposdasilva@hotmail.com>
2022-12-21 18:09:22 -0300

    Create product

--- CleanArch.MVC/Controllers/ProductController.cs
+++ CleanArch.MVC/Controllers/ProductController.cs
@@ -1,22 +1,42 @@
-﻿using CleanArch.Application.Interfaces;
+﻿using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArch.MVC.Controllers
 {
-    public class ProductController : Controller
+  public class ProductController : Controller
+  {
+    private readonly IProductService _productService;
+    private readonly ICategoryService _categoryService;
+
+    public ProductController(IProductService productService, ICategoryService categoryService)
     {
-        private readonly IProductService _productService;
-
-        public ProductController(IProductService productService)
-        {
-            _productService = productService;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            var products = await _productService.GetProductsAsync();
-            return View(products);
-        }
+      _productService = productService;
+      _categoryService = categoryService;
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+      var products = await _productService.GetProductsAsync();
+      return View(products);
+    }
+    [HttpGet()]
+    public async Task<IActionResult> Create()
+    {
+      ViewBag.CategoryId = new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+      return View();
+    }
+    [HttpPost()]
+    public async Task<IActionResult> Create(ProductDTO product)
+    {
+      if (ModelState.IsValid)
+      {
+        await _productService.CreateAsync(product);
+        return RedirectToAction(nameof(Index));
+      }
+      return View(product);
+    }
+  }
 }
