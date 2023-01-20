5d7c86d33e087c55e6f554a69167049c532a2852
Gabriel C Silva <gabriel.camposdasilva@hotmail.com>
2023-01-20 18:13:07 -0300

    Categories methods

---
+++ CleanArch.API/Controllers/CategoriesController.cs
@@ -0,0 +1,38 @@
+﻿using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+            if(categories == null)
+            {
+                return NotFound("Categories not found");
+            }
+            return Ok(categories);
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Get(int id)
+        {
+            var category = await _categoryService.GetCategoryById(id);
+            if (category == null)
+            {
+                return NotFound("Category not found");
+            }
+            return Ok(category);
+        }
+    }
+}
